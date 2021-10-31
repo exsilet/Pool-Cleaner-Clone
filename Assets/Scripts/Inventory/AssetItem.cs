39054056b6bb38fe68bebb8487571641fd8fae71
exsilet <exsilet@mail.ru>
2021-11-01 01:14:43 +0500

    Added mixing mechanics.
    Added mixing mechanics. Added the appearance of the object on the scene and the animation of pouring. Added a script to the bucket when implementing Drag and Drop, a color for mixing appeared. Added color mixing so far only 2. Added a pool to the scene.

--- Assets/Scripts/Inventory/AssetItem.cs
+++ Assets/Scripts/Inventory/AssetItem.cs
@@ -5,7 +5,14 @@ public class AssetItem : ScriptableObject, Iitem
 {
     [SerializeField] private string _name;
     [SerializeField] private Sprite _icon;
+    [SerializeField] private GameObject _bottle;
+    [SerializeField] private Color _color;
 
     public string Name => _name;
+
     public Sprite UIIcon => _icon;
+
+    public GameObject UIitem => _bottle;
+
+    public Color UIcolor => _color;
 }
